c1f2cb4ff2e7f1afb78e76ec2cf1b91024bbba2f
Praneetha217 <praneethamodiboyina@gmail.com>
2025-09-16 02:13:16 -0700

    Add files via upload

---
+++ pg1.dart
@@ -0,0 +1,6 @@
+void main()
+{
+  var firstname ="John";
+  var lastname ="Doe";
+  print("fullname is $firstname $lastname");
+}
---
+++ pg2.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  int num1=10;
+  int num2=3;
+  int sum=num1+num2;
+  int diff=num1-num2;
+  int mull=num1*num2;
+  double div=num1/num2;
+  print("the sum is $sum");
+  print("the diff is $diff");
+  print("the mull is $mull");
+  print("the div is $div");
+}
